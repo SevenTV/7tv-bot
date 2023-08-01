4a20662202f6b37cfa8f309434ddc5ddf0933230
bread <75878656+broadeditz@users.noreply.github.com>
2023-08-02 00:07:25 +0200

    Added connection struct, to help the manager handle many connections

---
+++ pkg/manager/connection.go
@@ -0,0 +1,120 @@
+package manager
+
+import (
+	"github.com/seventv/twitch-irc-reader/pkg/irc"
+	"strings"
+)
+
+var (
+	// ConnectionCapacity determines how many channels you can JOIN on a single connection,
+	// must be set before creating any connections
+	ConnectionCapacity = 50
+)
+
+// connection helps you manage a single IRC connection using some middleware,
+// to enable the IRCManager to manage lots of connections
+type connection struct {
+	client   *irc.Client
+	channels []*channel
+	// capacity determines how many channels can be joined on a connection
+	capacity  int
+	onMessage func(msg *irc.Message, err error)
+}
+
+// newConnection sets up a new connection with capacity as set in ConnectionCapacity
+func newConnection(user, oauth string) *connection {
+	return &connection{
+		client:   irc.New(user, oauth).WithCapabilities(irc.CapTags),
+		channels: []*channel{},
+		capacity: ConnectionCapacity,
+	}
+}
+
+func (c *connection) connect() error {
+	c.client.OnMessage(c.handleMessages)
+	return c.client.Connect()
+}
+
+func (c *connection) disconnect() {
+	// TODO: implement, should make a way to pass parted channels back to the manager after
+}
+
+func (c *connection) join() {
+	// TODO: implement
+}
+
+func (c *connection) part() {
+	// TODO: implement, should make a way to pass parted channels back to the manager after
+}
+
+func (c *connection) setOnMessage(cb func(msg *irc.Message, err error)) {
+	c.onMessage = cb
+}
+
+// this is middleware, needed to properly handle important incoming system messages like PING, JOIN & PART
+func (c *connection) handleMessages(msg *irc.Message, err error) {
+	// don't bother running the middleware if there's an error for the message
+	if err != nil {
+		c.onMessage(msg, err)
+		return
+	}
+	switch msg.GetType() {
+	case irc.Ping:
+		c.pong(msg)
+	case irc.Join:
+		c.onJoin(msg)
+	case irc.Part:
+		c.onPart(msg)
+	}
+	c.onMessage(msg, err)
+}
+
+func (c *connection) pong(msg *irc.Message) {
+	c.client.SendString("PONG" + parsePingPayload(msg.String()))
+}
+
+func (c *connection) onJoin(msg *irc.Message) {
+	// flag joined channels as isJoined = true
+	c.setChannelsJoined(msg.String(), true)
+}
+
+func (c *connection) onPart(msg *irc.Message) {
+	// TODO: implement logic for properly removing channel or attempt reconnect (signal back to manager?)
+	// flag parted channels as isJoined = false
+	c.setChannelsJoined(msg.String(), false)
+}
+
+func (c *connection) setChannelsJoined(data string, isJoined bool) {
+	for _, joined := range parseChannels(data) {
+		for _, ch := range c.channels {
+			if joined == ch.name {
+				ch.isJoined = isJoined
+				break
+			}
+		}
+	}
+}
+
+func parseChannels(data string) []string {
+	splits := strings.Split(data, " ")
+	if len(splits) == 0 {
+		return nil
+	}
+	// list of users is contained in the last split
+	last := splits[len(splits)-1]
+
+	result := []string{}
+	for _, user := range strings.Split(last, ",") {
+		// user should always start with #, if this is not the case, something is wrong
+		if !strings.HasPrefix(user, "#") {
+			continue
+		}
+
+		result = append(result, strings.TrimLeft(user, "#"))
+	}
+	return result
+}
+
+func parsePingPayload(data string) string {
+	return strings.TrimPrefix(data, "PING")
+}
---
+++ pkg/manager/connection_test.go
@@ -0,0 +1,168 @@
+package manager
+
+import (
+	"github.com/seventv/twitch-irc-reader/pkg/irc"
+	"reflect"
+	"testing"
+)
+
+func Test_connection_handleMessages(t *testing.T) {
+	joinMsg, _ := irc.ParseMessage(":justinfan77777!justinfan77777@justinfan77777.tmi.twitch.tv JOIN #forsen")
+	partMsg, _ := irc.ParseMessage(":justinfan77777!justinfan77777@justinfan77777.tmi.twitch.tv PART #forsen")
+	type fields struct {
+		client    *irc.Client
+		channels  []*channel
+		capacity  int
+		onMessage func(msg *irc.Message, err error)
+	}
+	type args struct {
+		msg *irc.Message
+		err error
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   []*channel
+	}{
+		{
+			name: "JOIN",
+			fields: fields{
+				channels: []*channel{
+					{name: "forsen"},
+					{name: "sodapoppin"},
+				},
+				onMessage: func(msg *irc.Message, err error) {},
+			},
+			args: args{
+				msg: joinMsg,
+				err: nil,
+			},
+			want: []*channel{
+				{
+					name:     "forsen",
+					isJoined: true,
+				},
+				{
+					name:     "sodapoppin",
+					isJoined: false,
+				},
+			},
+		},
+		{
+			name: "PART",
+			fields: fields{
+				channels: []*channel{
+					{
+						name:     "forsen",
+						isJoined: true,
+					},
+					{
+						name:     "sodapoppin",
+						isJoined: true,
+					},
+				},
+				onMessage: func(msg *irc.Message, err error) {},
+			},
+			args: args{
+				msg: partMsg,
+				err: nil,
+			},
+			want: []*channel{
+				{
+					name:     "forsen",
+					isJoined: false,
+				},
+				{
+					name:     "sodapoppin",
+					isJoined: true,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &connection{
+				client:    tt.fields.client,
+				channels:  tt.fields.channels,
+				capacity:  tt.fields.capacity,
+				onMessage: tt.fields.onMessage,
+			}
+			c.handleMessages(tt.args.msg, tt.args.err)
+			if !reflect.DeepEqual(c.channels, tt.want) {
+				t.Errorf("handleMessages() %v, result didn't equal what we expected!", tt.name)
+			}
+		})
+	}
+}
+
+func Test_parseChannels(t *testing.T) {
+	type args struct {
+		data string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "JOIN",
+			args: args{
+				data: ":justinfan77777!justinfan77777@justinfan77777.tmi.twitch.tv JOIN #sodapoppin",
+			},
+			want: []string{"sodapoppin"},
+		},
+		{
+			name: "PART",
+			args: args{
+				data: ":justinfan77777!justinfan77777@justinfan77777.tmi.twitch.tv PART #sodapoppin",
+			},
+			want: []string{"sodapoppin"},
+		},
+		{
+			name: "PARTMany",
+			args: args{
+				data: ":justinfan77777!justinfan77777@justinfan77777.tmi.twitch.tv PART #sodapoppin,#forsen",
+			},
+			want: []string{"sodapoppin", "forsen"},
+		},
+		{
+			name: "PartialMessage",
+			args: args{
+				data: ":justinfan77777!justinfan77777@justinfan77777.tmi.twitch.tv PART",
+			},
+			want: []string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseChannels(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseChannels() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parsePingPayload(t *testing.T) {
+	type args struct {
+		data string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "PING",
+			args: args{data: "PING :tmi.twitch.tv"},
+			want: " :tmi.twitch.tv",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parsePingPayload(tt.args.data); got != tt.want {
+				t.Errorf("parsePingPayload() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
