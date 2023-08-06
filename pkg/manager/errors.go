fb7c44b11dd8f1390c93edae569f2dd48357d9e8
bread <75878656+broadeditz@users.noreply.github.com>
2023-08-06 22:56:37 +0200

    New errors

--- pkg/manager/errors.go
+++ pkg/manager/errors.go
@@ -3,6 +3,8 @@ package manager
 import "errors"
 
 var (
+	// ErrManagerClosing means you're trying to perform an action on an IRCManager after Shutdown() was called
+	ErrManagerClosing = errors.New("manager is shutting down")
 	// ErrConnNotFound means the connection you're trying to perform an operation on does not exist
 	ErrConnNotFound = errors.New("connection not found")
 	// ErrChanAlreadyJoined means the channel you're trying to join is already active
@@ -11,4 +13,6 @@ var (
 	ErrChanNotFound = errors.New("ircChannel not found")
 	// ErrNoCapacity is returned when you're trying to join a channel on a connection that doesn't have the needed capacity
 	ErrNoCapacity = errors.New("no remaining capacity on the connection")
+	// ErrOnMessageUnset means the manager does not have a callback for OnMessage set
+	ErrOnMessageUnset = errors.New("OnMessage has not been set")
 )
