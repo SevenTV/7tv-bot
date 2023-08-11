887f169169e54ea4b00a3f2085be2b07aea8fd1e
bread <75878656+broadeditz@users.noreply.github.com>
2023-08-11 18:28:57 +0200

    Added type for channels stored in mongo

---
+++ pkg/types/channel.go
@@ -0,0 +1,15 @@
+package types
+
+import "time"
+
+// Channel is the struct we use to decode data from mongo
+// TODO: keep track of username changes/history?
+type Channel struct {
+	ID        int64     `bson:"user_id"`
+	Flags     uint32    `bson:"flags,omitempty"`
+	Username  string    `bson:"username"`
+	Platform  string    `bson:"platform"`
+	Weight    uint      `bson:"weight"`
+	CreatedAt time.Time `bson:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at"`
+}
