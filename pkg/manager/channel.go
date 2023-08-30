9183a7ef114542f902153e368d7ce47ad96242a8
bread <75878656+broadeditz@users.noreply.github.com>
2023-08-30 15:09:19 +0200

    Exported NewIrcChannel function

--- pkg/manager/channel.go
+++ pkg/manager/channel.go
@@ -15,10 +15,10 @@ type IRCChannel struct {
 	connectionKey uint
 }
 
-// newIrcChannel returns a new IRCChannel with given name & weight.
+// NewIrcChannel returns a new IRCChannel with given name & weight.
 // Weight determines how much capacity the channel takes on the connection.
 // By default, any weight value of 50 or higher, will create a connection just for this channel alone.
-func newIrcChannel(name string, weight int) *IRCChannel {
+func NewIrcChannel(name string, weight int) *IRCChannel {
 	if weight > ConnectionCapacity {
 		weight = ConnectionCapacity
 	}
