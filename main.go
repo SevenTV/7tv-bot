53e8101cf4b05654f786382e6e85e4f95bbd2534
bread <75878656+broadeditz@users.noreply.github.com>
2023-09-21 18:32:37 +0200

    Added main.go

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.uber.org/zap"
+
+	"github.com/seventv/twitch-irc-reader/config"
+	"github.com/seventv/twitch-irc-reader/internal/database"
+	"github.com/seventv/twitch-irc-reader/internal/service"
+)
+
+func main() {
+	logger, _ := zap.NewProduction()
+	zap.ReplaceGlobals(logger)
+
+	cfg := config.New()
+
+	err := database.Connect(cfg.Mongo.ConnectionString, cfg.Mongo.Database)
+	if err != nil {
+		panic(err)
+	}
+	database.EnsureCollection(cfg.Mongo.Collection)
+
+	svc := service.New(cfg)
+	err = svc.Init()
+	if err != nil {
+		panic(err)
+	}
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case <-shutdown:
+		zap.L().Info("Shutting down...")
+		svc.Shutdown()
+	}
+}
