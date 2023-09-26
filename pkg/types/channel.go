7432e7d619e5a4e92d7852a1cc1f3defe57f581c
bread <75878656+broadeditz@users.noreply.github.com>
2023-09-26 18:25:27 +0200

    Added JSON tags for channel struct

--- pkg/types/channel.go
+++ pkg/types/channel.go
@@ -5,11 +5,11 @@ import "time"
 // Channel is the struct we use to decode data from mongo
 // TODO: keep track of username changes/history?
 type Channel struct {
-	ID        int64     `bson:"user_id"`
-	Flags     uint32    `bson:"flags,omitempty"`
-	Username  string    `bson:"username"`
-	Platform  string    `bson:"platform"`
-	Weight    int       `bson:"weight"`
-	CreatedAt time.Time `bson:"created_at"`
-	UpdatedAt time.Time `bson:"updated_at"`
+	ID        int64     `bson:"user_id" json:"user_id"`
+	Flags     uint32    `bson:"flags,omitempty" json:"flags"`
+	Username  string    `bson:"username" json:"username"`
+	Platform  string    `bson:"platform" json:"platform"`
+	Weight    int       `bson:"weight" json:"weight"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
 }
