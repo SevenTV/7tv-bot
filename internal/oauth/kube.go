89d9fe8b0fc1b22564f6014c2039162fdcbe92ac
bread <75878656+broadeditz@users.noreply.github.com>
2023-09-27 20:14:28 +0200

    Push OAuth & refresh tokens to kubernetes secrets

---
+++ internal/oauth/kube.go
@@ -0,0 +1,33 @@
+package oauth
+
+import (
+	"context"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/client-go/applyconfigurations/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func (s *Service) kubeInit() error {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return err
+	}
+	s.kube, err = kubernetes.NewForConfig(config)
+	return err
+}
+
+func (s *Service) setKubeSecret(ctx context.Context, auth *OauthResponse) error {
+	secret := v1.Secret(s.cfg.Kube.Oauthsecret, s.cfg.Kube.Namespace).
+		WithType("Opaque")
+	secret.StringData = make(map[string]string)
+	secret.StringData["access-token"] = auth.AccessToken
+	secret.StringData["refresh-token"] = auth.RefreshToken
+
+	opts := metav1.ApplyOptions{}
+	opts.FieldManager = "7tv-auth"
+
+	_, err := s.kube.CoreV1().Secrets(s.cfg.Kube.Namespace).Apply(ctx, secret, opts)
+	return err
+}
