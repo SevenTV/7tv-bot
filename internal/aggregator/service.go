25ad4fdaa037e0356d1c0f2776af8aca47d8e29b
bread <75878656+broadeditz@users.noreply.github.com>
2023-10-04 14:06:21 +0200

    Initialize mongo

--- internal/aggregator/service.go
+++ internal/aggregator/service.go
@@ -3,7 +3,12 @@ package aggregator
 import (
 	"context"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+
 	"github.com/seventv/7tv-bot/internal/aggregator/config"
+	"github.com/seventv/7tv-bot/pkg/database"
+	emotedb "github.com/seventv/7tv-bot/pkg/database/emotes"
 )
 
 type Service struct {
@@ -15,7 +20,16 @@ func New(cfg *config.Config) *Service {
 }
 
 func (s *Service) Init() error {
-	// TODO: database
+	database.Connect(s.cfg.Mongo.ConnectionString, s.cfg.Mongo.Database)
+	coll := database.EnsureCollection(
+		s.cfg.Mongo.Collection,
+		[]mongo.IndexModel{
+			{Keys: bson.D{{"emote_id", -1}}},
+			{Keys: bson.D{{"count", -1}}},
+			{Keys: bson.D{{"flags", -1}, {"count", -1}}},
+		},
+	)
+	emotedb.SetCollections(coll)
 	err := s.subscribe(context.TODO(), s.handleMessage)
 	if err != nil {
 		return err
