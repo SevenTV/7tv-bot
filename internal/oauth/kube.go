012af4def8aecfeb6c22574b586caeaafea5b8bf
bread <75878656+broadeditz@users.noreply.github.com>
2023-10-05 02:34:22 +0200

    add oauth: in front of access token

--- internal/oauth/kube.go
+++ internal/oauth/kube.go
@@ -2,6 +2,7 @@ package oauth
 
 import (
 	"context"
+	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/client-go/applyconfigurations/core/v1"
@@ -22,7 +23,7 @@ func (s *Service) setKubeSecret(ctx context.Context, auth *OauthResponse) error 
 	secret := v1.Secret(s.cfg.Kube.Oauthsecret, s.cfg.Kube.Namespace).
 		WithType("Opaque")
 	secret.StringData = make(map[string]string)
-	secret.StringData["access-token"] = auth.AccessToken
+	secret.StringData["access-token"] = fmt.Sprintf("oauth:%v", auth.AccessToken)
 	secret.StringData["refresh-token"] = auth.RefreshToken
 
 	opts := metav1.ApplyOptions{}
