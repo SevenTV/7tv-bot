76cde29df525acb97574e0a91a16c4143dc51dd6
bread <75878656+broadeditz@users.noreply.github.com>
2023-10-11 14:52:22 +0200

    change emote types

--- pkg/types/emote.go
+++ pkg/types/emote.go
@@ -1,19 +1,27 @@
 package types
 
 import (
+	"time"
+
 	"github.com/seventv/api/data/model"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type CountedEmote struct {
-	Count int
-	Emote Emote
+type Emote struct {
+	Name    string                     `json:"name"`
+	EmoteID primitive.ObjectID         `json:"emote_id"`
+	Flags   model.ActiveEmoteFlagModel `json:"flags"`
+	State   []model.EmoteVersionState  `json:"state,omitempty"`
+	URL     string                     `json:"url"`
 }
 
-type Emote struct {
-	Name    string                     `bson:"name"`
-	EmoteID primitive.ObjectID         `bson:"emote_id"`
-	Flags   model.ActiveEmoteFlagModel `bson:"flags"`
-	State   []model.EmoteVersionState  `bson:"state,omitempty"`
-	URL     string                     `bson:"url"`
+type EmoteCount struct {
+	Name      string                     `bson:"name" json:"name"`
+	EmoteID   primitive.ObjectID         `bson:"emote_id" json:"emote_id"`
+	Flags     model.ActiveEmoteFlagModel `bson:"flags,omitempty" json:"flags,omitempty"`
+	State     []model.EmoteVersionState  `bson:"state,omitempty" json:"state,omitempty"`
+	URL       string                     `bson:"url" json:"url"`
+	CreatedAt *time.Time                 `bson:"created_at" json:"created_at,omitempty"`
+	UpdatedAt *time.Time                 `bson:"updated_at" json:"updated_at,omitempty"`
+	Count     int                        `bson:"count" json:"count"`
 }
