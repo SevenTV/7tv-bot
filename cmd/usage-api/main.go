885a0eee5972923189b9396c2f4621a89295dfd4
bread <75878656+broadeditz@users.noreply.github.com>
2023-10-11 14:51:32 +0200

    add debug loglevel

--- cmd/usage-api/main.go
+++ cmd/usage-api/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"go.uber.org/zap"
+
 	usage_api "github.com/seventv/7tv-bot/internal/usage-api"
 	"github.com/seventv/7tv-bot/internal/usage-api/config"
 )
 
 func main() {
+	logger, _ := zap.NewProduction()
+	zap.ReplaceGlobals(logger)
 	cfg := config.New()
+	if cfg.LogLevel == "debug" {
+		logger, _ = zap.NewDevelopment()
+		zap.ReplaceGlobals(logger)
+	}
+	zap.S().Debug("using debug logger")
+
 	svc := usage_api.New(cfg)
 	svc.Init()
 	select {}
